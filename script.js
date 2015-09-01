1b624dc80548fb7bf0f96c58847dd7b3f9258a13
Mitch Conquer <mitch@robotshanghai.com>
2015-09-01 12:03:08 +0800

    Fixed spacing between pixels so now generates an even grid

--- script.js
+++ script.js
@@ -13,14 +13,15 @@ NOTES:
 $(document).ready(function(){
 	var pixels = 16; // initial pixel density
 	initializeGrid(pixels);
+
+	// $(document).on('mouseenter', '.pixel', changeColor());
 });
 
 function initializeGrid(pixels){
 	clearGrid();
 	var width = gridWidth();
 	var args = {'gridWidth': width, 'pixels': pixels};
-	var pixelSize = calculatePixelSize(args);
-	args['pixelSize'] = pixelSize;
+	args['pixelSize'] = calculatePixelSize(args);
 	drawGrid(args);
 }
 
@@ -41,10 +42,10 @@ function drawGrid(args){
 	var pixels = args['pixels'];
 	var width = args['gridWidth'];
 	var pixelSize = args['pixelSize'];
-	var pixel = '<div class="pixel" style="width: ' + pixelSize + 'px; height: ' + pixelSize+ 'px"></div>';
+	var pixel = '<div class="pixel" style="width: ' + pixelSize + 'px; height: ' + (pixelSize - 3) + 'px"></div>';
 
 	for (i = 0; i < pixels * pixels; i++) {
-		$('#grid').append(pixel);
+		$('#grid').append(pixel).fadeTo('fast', 1);
 	}
 }
 
